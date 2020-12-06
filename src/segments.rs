b1ecad0f942517db05a075cb02b425e6773b87e6
Ralf Fuest <mail@rfuest.de>
2020-12-06 21:07:39 +0100

    Add additional characters

--- src/segments.rs
+++ src/segments.rs
@@ -29,6 +29,7 @@ impl TryFrom<char> for Segments {
 
     fn try_from(value: char) -> Result<Self, Self::Error> {
         Ok(match value {
+            ' ' => Self::empty(),
             '0' => Self::A | Self::B | Self::C | Self::D | Self::E | Self::F,
             '1' => Self::B | Self::C,
             '2' => Self::A | Self::B | Self::D | Self::E | Self::G,
@@ -39,8 +40,169 @@ impl TryFrom<char> for Segments {
             '7' => Self::A | Self::B | Self::C,
             '8' => Self::A | Self::B | Self::C | Self::D | Self::E | Self::F | Self::G,
             '9' => Self::A | Self::B | Self::C | Self::D | Self::F | Self::G,
-            // TODO: add hex digits
+            'a' | 'A' => Self::A | Self::B | Self::C | Self::E | Self::F | Self::G,
+            'b' | 'B' => Self::C | Self::D | Self::E | Self::F | Self::G,
+            'c' => Self::D | Self::E | Self::G,
+            'C' => Self::A | Self::D | Self::E | Self::F,
+            'd' | 'D' => Self::B | Self::C | Self::D | Self::E | Self::G,
+            'e' | 'E' => Self::A | Self::D | Self::E | Self::F | Self::G,
+            'f' | 'F' => Self::A | Self::E | Self::F | Self::G,
+            'g' | 'G' => Self::A | Self::C | Self::D | Self::E | Self::F,
+            'h' => Self::C | Self::E | Self::F | Self::G,
+            'H' => Self::B | Self::C | Self::E | Self::F | Self::G,
+            'i' | 'I' => Self::E | Self::F,
+            'j' | 'J' => Self::B | Self::C | Self::D | Self::E,
+            'l' | 'L' => Self::D | Self::E | Self::F,
+            'n' | 'N' => Self::C | Self::E | Self::G,
+            'o' => Self::C | Self::D | Self::E | Self::G,
+            'O' => Self::A | Self::B | Self::C | Self::D | Self::E | Self::F,
+            'p' | 'P' => Self::A | Self::B | Self::E | Self::F | Self::G,
+            'q' | 'Q' => Self::A | Self::B | Self::C | Self::F | Self::G,
+            'r' | 'R' => Self::E | Self::G,
+            's' | 'S' => Self::A | Self::C | Self::D | Self::F | Self::G,
+            't' | 'T' => Self::D | Self::E | Self::F | Self::G,
+            'u' => Self::C | Self::D | Self::E,
+            'U' => Self::B | Self::C | Self::D | Self::E | Self::F,
+            'y' | 'Y' => Self::B | Self::C | Self::D | Self::F | Self::G,
+            '_' => Self::D,
+            '-' => Self::G,
+            '=' => Self::D | Self::G,
+            '°' => Self::A | Self::B | Self::F | Self::G,
+            '"' => Self::B | Self::F,
+            '\'' => Self::F,
+            '(' | '[' => Self::A | Self::D | Self::E | Self::F,
+            ')' | ']' => Self::A | Self::B | Self::C | Self::D,
+            '?' => Self::A | Self::B | Self::E | Self::G,
+            // TODO: add https://en.wikipedia.org/wiki/Symbols_for_Legacy_Computing ?
             _ => return Err(()),
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::SevenSegmentTextStyleBuilder;
+    use embedded_graphics::{
+        fonts::Text, mock_display::MockDisplay, pixelcolor::BinaryColor, prelude::*,
+    };
+
+    fn test_segments(text: &str, expected_pattern: &[&str]) {
+        let style = SevenSegmentTextStyleBuilder::new()
+            .digit_size(Size::new(5, 7))
+            .digit_spacing(1)
+            .segment_width(1)
+            .segment_color(BinaryColor::On)
+            .build();
+
+        let mut display = MockDisplay::new();
+        Text::new(text, Point::zero())
+            .into_styled(style)
+            .draw(&mut display)
+            .unwrap();
+
+        display.assert_pattern(expected_pattern);
+    }
+
+    #[test]
+    fn digits() {
+        test_segments(
+            "0123456789",
+            &[
+                " ###         ###   ###         ###   ###   ###   ###   ### ",
+                "#   #     #     #     # #   # #     #         # #   # #   #",
+                "#   #     #     #     # #   # #     #         # #   # #   #",
+                "             ###   ###   ###   ###   ###         ###   ### ",
+                "#   #     # #         #     #     # #   #     # #   #     #",
+                "#   #     # #         #     #     # #   #     # #   #     #",
+                " ###         ###   ###         ###   ###         ###   ### ",
+            ],
+        );
+    }
+
+    #[test]
+    fn lower_case_letters() {
+        test_segments(
+            "abcdefghij",
+            &[
+                " ###                     ###   ###   ###                   ",
+                "#   # #               # #     #     #     #     #         #",
+                "#   # #               # #     #     #     #     #         #",
+                " ###   ###   ###   ###   ###   ###         ###             ",
+                "#   # #   # #     #   # #     #     #   # #   # #     #   #",
+                "#   # #   # #     #   # #     #     #   # #   # #     #   #",
+                "       ###   ###   ###   ###         ###               ### ",
+            ],
+        );
+
+        test_segments(
+            "lnopqrstuy",
+            &[
+                "                   ###   ###         ###                   ",
+                "#                 #   # #   #       #     #           #   #",
+                "#                 #   # #   #       #     #           #   #",
+                "       ###   ###   ###   ###   ###   ###   ###         ### ",
+                "#     #   # #   # #         # #         # #     #   #     #",
+                "#     #   # #   # #         # #         # #     #   #     #",
+                " ###         ###                     ###   ###   ###   ### ",
+            ],
+        );
+    }
+
+    #[test]
+    fn upper_case_letters() {
+        test_segments(
+            "ABCDEFGHIJ",
+            &[
+                " ###         ###         ###   ###   ###                   ",
+                "#   # #     #         # #     #     #     #   # #         #",
+                "#   # #     #         # #     #     #     #   # #         #",
+                " ###   ###         ###   ###   ###         ###             ",
+                "#   # #   # #     #   # #     #     #   # #   # #     #   #",
+                "#   # #   # #     #   # #     #     #   # #   # #     #   #",
+                "       ###   ###   ###   ###         ###               ### ",
+            ],
+        );
+
+        test_segments(
+            "LNOPQRSTUY",
+            &[
+                "             ###   ###   ###         ###                   ",
+                "#           #   # #   # #   #       #     #     #   # #   #",
+                "#           #   # #   # #   #       #     #     #   # #   #",
+                "       ###         ###   ###   ###   ###   ###         ### ",
+                "#     #   # #   # #         # #         # #     #   #     #",
+                "#     #   # #   # #         # #         # #     #   #     #",
+                " ###         ###                     ###   ###   ###   ### ",
+            ],
+        );
+    }
+
+    #[test]
+    fn other_chars() {
+        test_segments(
+            " _-=°\"'",
+            &[
+                "                         ###             ",
+                "                        #   # #   # #    ",
+                "                        #   # #   # #    ",
+                "             ###   ###   ###             ",
+                "                                         ",
+                "                                         ",
+                "       ###         ###                   ",
+            ],
+        );
+
+        test_segments(
+            "([])?",
+            &[
+                " ###   ###   ###   ###   ### ",
+                "#     #         #     #     #",
+                "#     #         #     #     #",
+                "                         ### ",
+                "#     #         #     # #    ",
+                "#     #         #     # #    ",
+                " ###   ###   ###   ###       ",
+            ],
+        );
     }
 }
